fdb9794bd0c35a8e527de1154666d5f66829ed9a
Dustin Lacewell <dlacewell@gmail.com>
2020-03-26 06:36:44 -0500

    Initial commit

---
+++ rekor.el
@@ -0,0 +1,19 @@
+;;; rekor.el --- An ORM over EmacSQL -*- lexical-binding: t; -*-
+;; Copyright (C) 2020 Dustin Lacewell
+
+;; Author: Dustin Lacewell <dlacewell@gmail.com>
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "26") (emacsql-sqlite "0"))
+;; Keywords: org-mode formatting
+;; URL: http://github.com/apoptosis/rekor.el
+
+;;; Commentary:
+
+;; This package provides an ORM over EmacSQL
+
+;;; Code:
+(require 'emacsql-sqlite)
+
+(provide 'rekor)
+
+;;; rekor.el ends here
